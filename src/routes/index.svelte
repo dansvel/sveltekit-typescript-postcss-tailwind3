278e9d9ca9f3ddcd5a13daf85080262a9840132e
dan <48936611+dansvel@users.noreply.github.com>
2021-01-31 23:16:36 +0700

    📝 add meta tag

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,13 +5,35 @@
   import Timer from '../components/Timer.svelte';
 </script>
 
+<svelte:head>
+  <!-- Primary Meta Tags -->
+  <title>SvelteKit ❤️ Tailwind 2</title>
+  <meta name="title" content="SvelteKit ❤️ Tailwind 2">
+  <meta name="description" content="SvelteKit and Tailwind 2 toggle dark/light theme demo">
+  <meta name="keywords" content="Svelte, SvelteKit, Svelte-kit, Tailwind, HTML, CSS, JavaScript">
+  <meta name="author" content="@dansvel">
+  <!-- Open Graph / Facebook -->
+  <meta property="og:type" content="website">
+  <meta property="og:url" content="https://metatags.io/">
+  <meta property="og:title" content="SvelteKit ❤️ Tailwind 2">
+  <meta property="og:description" content="SvelteKit and Tailwind 2 toggle dark/light theme demo">
+  <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
+
+  <!-- Twitter -->
+  <meta property="twitter:card" content="summary_large_image">
+  <meta property="twitter:url" content="https://metatags.io/">
+  <meta property="twitter:title" content="SvelteKit ❤️ Tailwind 2">
+  <meta property="twitter:description" content="SvelteKit and Tailwind 2 toggle dark/light theme demo">
+  <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
+</svelte:head>
+
 <div>
   <h1 class="text-5xl font-extrabold mb-4">Hello {$theme} world!</h1>
 
   <ToggleTheme />
 
   <blockquote class="text-xl p-2 bg-gray-200 dark:bg-gray-800 mb-2">
-    For more information, check <a href="https://github.com/dansvel/sveltekit-typescript-postcss-tailwind2"
+    <a href="https://github.com/dansvel/sveltekit-typescript-postcss-tailwind2"
       ><b>Github repo</b></a>
     or <a href="https://dev.to/dansvel/sveltekit-svelte-next-with-tailwind-2-4dnn"><b>dev.to post</b></a>
   </blockquote>
