c4ec8c02d6af83a35e2b953d3acba7661067b96f
dan <48936611+dansvel@users.noreply.github.com>
2021-02-04 11:01:25 +0700

    🩹 fix future issue

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -27,28 +27,23 @@
   <meta property="twitter:image" content="https://dev-to-uploads.s3.amazonaws.com/i/yyqy860f5z4n4vk8euzz.png">
 </svelte:head>
 
-<div>
-  <h1 class="text-5xl font-extrabold mb-4">Hello {$theme} world!</h1>
+<h1 class="text-5xl font-extrabold mb-4">Hello {$theme} world!</h1>
 
-  <ToggleTheme />
+<ToggleTheme />
 
-  <blockquote class="text-xl p-2 bg-gray-200 dark:bg-gray-800 mb-2">
-    <a href="https://github.com/dansvel/sveltekit-typescript-postcss-tailwind2"
-      ><b>Github repo</b></a>
-    or <a href="https://dev.to/dansvel/sveltekit-svelte-next-with-tailwind-2-4dnn"><b>dev.to post</b></a>
-  </blockquote>
-  <p>Visit the <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte apps.</p>
-  <p>The title is using utility class, but all other elements is using style component.</p>
-  <p>Just check <code>src/routes/index.svelte</code> to see the code.</p>
+<blockquote class="text-xl p-2 bg-gray-200 dark:bg-gray-800 mb-2">
+  <a href="https://github.com/dansvel/sveltekit-typescript-postcss-tailwind2"
+    ><b>Github repo</b></a>
+  or <a href="https://dev.to/dansvel/sveltekit-svelte-next-with-tailwind-2-4dnn"><b>dev.to post</b></a>
+</blockquote>
+<p>Visit the <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte apps.</p>
+<p>The title is using utility class, but all other elements is using style component.</p>
+<p>Just check <code>src/routes/index.svelte</code> to see the code.</p>
 
-  <Counter />
-  <Timer />
-</div>
+<Counter />
+<Timer />
 
 <style>
-  div {
-    @apply text-center;
-  }
   p {
     @apply text-lg;
     @apply mt-0 mb-1;
