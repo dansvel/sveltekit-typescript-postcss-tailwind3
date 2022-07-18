d927231bbe35f5764e3b7d0f341ad1a8b6f0f236
dan <dan.surat@outlook.co.id>
2022-07-19 00:34:53 +0700

    Update index.svelte
    change to tailwind 3

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,16 +7,16 @@
 
 <svelte:head>
   <!-- Primary Meta Tags -->
-  <title>SvelteKit ❤️ Tailwind 2</title>
-  <meta name="title" content="SvelteKit ❤️ Tailwind 2" />
-  <meta name="description" content="SvelteKit and Tailwind 2 toggle dark/light theme demo" />
+  <title>SvelteKit ❤️ Tailwind 3</title>
+  <meta name="title" content="SvelteKit ❤️ Tailwind 3" />
+  <meta name="description" content="SvelteKit and Tailwind 3 toggle dark/light theme demo" />
   <meta name="keywords" content="Svelte, SvelteKit, Svelte-kit, Tailwind, HTML, CSS, JavaScript" />
   <meta name="author" content="@dansvel" />
   <!-- Open Graph / Facebook -->
   <meta property="og:type" content="website" />
   <meta property="og:url" content="https://metatags.io/" />
-  <meta property="og:title" content="SvelteKit ❤️ Tailwind 2" />
-  <meta property="og:description" content="SvelteKit and Tailwind 2 toggle dark/light theme demo" />
+  <meta property="og:title" content="SvelteKit ❤️ Tailwind 3" />
+  <meta property="og:description" content="SvelteKit and Tailwind 3 toggle dark/light theme demo" />
   <meta
     property="og:image"
     content="https://sveltekit-tailwind2.netlify.app/screenshot.png"
@@ -25,10 +25,10 @@
   <!-- Twitter -->
   <meta property="twitter:card" content="summary_large_image" />
   <meta property="twitter:url" content="https://metatags.io/" />
-  <meta property="twitter:title" content="SvelteKit ❤️ Tailwind 2" />
+  <meta property="twitter:title" content="SvelteKit ❤️ Tailwind 3" />
   <meta
     property="twitter:description"
-    content="SvelteKit and Tailwind 2 toggle dark/light theme demo"
+    content="SvelteKit and Tailwind 3 toggle dark/light theme demo"
   />
   <meta
     property="twitter:image"
